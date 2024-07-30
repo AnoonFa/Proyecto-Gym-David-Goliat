c72ffcb33a8d5c63ce5b63d6fdcd8a14d8d30261
ElPinasXD <estebanpineros42@gmail.com>
2024-07-29 21:28:53 -0500

    correccion

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Plans from './components/Plans/Plans'
-import PlanesCliente from './pages/Client/PlansPage/PlansClient';
+/*import PlanesCliente from './pages/Client/PlansPage/PlansClient';
 import PlanesAdmin from './pages/Management/ManagePlans/PlansAdmi';
 import ClientPayments from './pages/Client/PaymentsPage/ClienPayments';
 import AdminPayments from './pages/Management/ManagementPayments/AdminPayments';
 import Register from './pages/Auth/RegisterPage/Register';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
-import RoleSelector from './components/RoleSelector/RoleSelector';
+import RoleSelector from './components/RoleSelector/RoleSelector';*/
 import Carousel from './components/Carousel/Carousel';
 import Products from './components/Products/Products';
 import AdminPage from './components/adminEmpleadoIndex/adminEmpleadoIndex';
@@ -28,7 +28,6 @@ function App() {
               <>
                 <Header />
                 <Carousel />
-                <Register />
                 <Plans />
                 <Products />
                 <Footer />
